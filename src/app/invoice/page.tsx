68c5389167a33fc0ce12dfad85e22fba87e5650b
Muhammad H. Shahzad <mh.shahzad@outlook.com>
2024-06-29 17:04:23 +0300

    feat(main): add fields to the invoice page

--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -1,8 +1,20 @@
+import {Button, FormLabel, Grid, Input} from "@mui/joy";
+
 const InvoicePage = () => {
     return (
-        <div>
-        <h1>Invoice Page</h1>
-        </div>
+        <Grid container spacing={2} p={4}>
+            <Grid xs={12} md={4} sm={12}>
+                <FormLabel>Select Start Date: </FormLabel>
+                <Input type={'date'}></Input>
+                <FormLabel>Select End Date: </FormLabel>
+                <Input type={'date'}></Input>
+                <FormLabel>Invoice Number </FormLabel>
+                <Input type={'number'}></Input>
+                <FormLabel>Month </FormLabel>
+                <Input type={'month'}></Input>
+                <Button fullWidth>Generate</Button>
+            </Grid>
+        </Grid>
     );
 }
 
